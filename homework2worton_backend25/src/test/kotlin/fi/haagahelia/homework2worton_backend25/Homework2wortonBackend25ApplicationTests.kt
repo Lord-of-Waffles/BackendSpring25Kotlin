df8b7bf5a5d22c40006d54c8a15f17d7977fa03d
Lord-of-Waffles <ben.worton@gmail.com>
2025-01-23 08:32:56 +0200

    added new project

---
+++ homework2worton_backend25/src/test/kotlin/fi/haagahelia/homework2worton_backend25/Homework2wortonBackend25ApplicationTests.kt
@@ -0,0 +1,13 @@
+package fi.haagahelia.homework2worton_backend25
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Homework2wortonBackend25ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
