33a32a900cee573ae756d0a234fe9716034304d2
Lord-of-Waffles <ben.worton@gmail.com>
2025-02-05 17:37:28 +0200

    Exercise 2

--- bookstore/src/main/kotlin/com/example/bookstore/domain/Book.kt
+++ bookstore/src/main/kotlin/com/example/bookstore/domain/Book.kt
@@ -9,9 +9,11 @@ data class Book(
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null,  // Nullable ID for auto-generation
 
-    var title: String,
-    var author: String,
-    var publicationYear: Int,
-    var isbn: String,
-    var price: Double
-)
+    var title: String = "",
+    var author: String = "",
+    var publicationYear: Int = 0,
+    var isbn: String = "",
+    var price: Double = 0.0
+) {
+    constructor() : this(null, "", "", 0, "", 0.0)
+}
--- bookstore/src/main/kotlin/com/example/bookstore/web/BookController.kt
+++ bookstore/src/main/kotlin/com/example/bookstore/web/BookController.kt
@@ -1,12 +1,17 @@
 package com.example.bookstore.web
 
 import com.example.bookstore.domain.Book
+import com.example.bookstore.domain.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class BookController {
+
+    @Autowired
+    private lateinit var repository: BookRepository
 
     @GetMapping("/index")
     fun index(model: Model): String {
@@ -16,5 +21,11 @@ class BookController {
         )
         model.addAttribute("books", books)
         return "index"
+    }
+
+    @GetMapping("/booklist")
+    fun booklist(model: Model): String {
+        model.addAttribute("books", repository.findAll())
+        return "booklist"
     }
 }
